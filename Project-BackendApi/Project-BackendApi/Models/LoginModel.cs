3c8f68c982912b2b1acf0f5c16be3ddea94974a3
Sahanaka <weraniyagoda97@gmail.com>
2020-11-13 13:21:59 +0530

    Login model Added

---
+++ Project-BackendApi/Project-BackendApi/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Project_BackendApi.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Cannot leave this field empty")]
+        public string Password { get; set; }
+    }
+}
