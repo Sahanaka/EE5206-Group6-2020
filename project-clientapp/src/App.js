a233bf1ad4690acef0294095a875212f6cfbc81c
Udara Sudeepa <59205501+Usmanathunga@users.noreply.github.com>
2021-01-14 23:12:06 +0530

    Implemented nav bar

--- project-clientapp/src/App.js
+++ project-clientapp/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import "./App.css";
+import Background from "./img/mainimg.JPG";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div style={{ backgroundImage: `url(${Background})` }}></div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          {/* <Route exact path="/login" component={login} />
+          <Route exact path="/Register" component={Register} /> */}
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
