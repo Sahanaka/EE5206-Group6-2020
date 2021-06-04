bf17b857a351f33637b9cf48cc1f061aa4467ea6
Usmanathunga <udarasudeepa8@gmail.com>
2021-06-04 20:34:45 +0530

    Seller Dashbord

--- project-clientapp/src/index.js
+++ project-clientapp/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+//import '@fortawesome/fontawesome-free/css/all.css';
 
 ReactDOM.render(
   <React.StrictMode>
