e490fe2fd92c145a22a2b55baeab8be42b200fa4
sahanaka <weraniyagoda97@gmail.com>
2021-06-05 19:50:24 +0530

    Working on admin controller

---
+++ Project-BackendApi/Project-BackendApi/Services/AdminService/AdminService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project_BackendApi.DATA;
+
+namespace Project_BackendApi.Services.AdminService
+{
+    public class AdminService : IAdminService
+    {
+        private readonly MarketplaceDB _db;
+
+        public AdminService(MarketplaceDB db)
+        {
+            _db = db;
+        }
+        public List<string> GetData() 
+        {
+            // All the data needded to be displayed in the dashboard. (No users, NO cus, No sellers)
+            // public List<string> DataList = new List<string>();
+
+
+            // _db.
+            return null;
+              
+        }
+
+        public Task DeleteUser(int UserId, string UserRole) { return null; }
+    }
+}
---
+++ Project-BackendApi/Project-BackendApi/Services/AdminService/IAdminService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_BackendApi.Services.AdminService
+{
+    public interface IAdminService
+    {
+        public List<string> GetData();
+
+        public Task DeleteUser(int UserId, string UserRole);
+    }
+}
